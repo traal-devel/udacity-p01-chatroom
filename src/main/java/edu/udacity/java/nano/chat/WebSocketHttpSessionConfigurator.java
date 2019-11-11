c215986b79028d4d3c4bf063e3e040c7d1284335
devel@traal.ch <devel@traal.ch>
2019-11-11 22:19:16 +0100

    feat: (Experiment) Add ServerEndpointConfig.Configurator to implement a

---
+++ src/main/java/edu/udacity/java/nano/chat/WebSocketHttpSessionConfigurator.java
@@ -0,0 +1,51 @@
+package edu.udacity.java.nano.chat;
+
+import javax.servlet.http.HttpSession;
+import javax.websocket.HandshakeResponse;
+import javax.websocket.server.HandshakeRequest;
+import javax.websocket.server.ServerEndpointConfig;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * (Experiment) WebSocketHttpSessionConfigurator used to check if an valid
+ * access_token / http-session exists. 
+ * 
+ * @author traal-devel
+ */
+public class WebSocketHttpSessionConfigurator 
+                                extends ServerEndpointConfig.Configurator  {
+
+  
+  /* constants */
+  private static final Logger logger = LogManager.getLogger(WebSocketHttpSessionConfigurator.class);
+  
+  /* member variables */
+  
+  
+  /* constructors */
+  public WebSocketHttpSessionConfigurator() {
+    super();
+  }
+  
+  
+  /* methods */
+  @Override
+  public void modifyHandshake(
+      ServerEndpointConfig sec, 
+      HandshakeRequest request, 
+      HandshakeResponse response
+  ) {
+    logger.debug("modifyHandshake() invoked.");
+    
+    HttpSession session = (HttpSession)request.getHttpSession();
+    if (session != null) {
+      // :TODO: If access token is not exist then return an exception
+      sec.getUserProperties().put(
+          WebSocketConfig.PROP_KEY_ACCESS_TOKEN, 
+          session.getAttribute(WebSocketConfig.PROP_KEY_ACCESS_TOKEN));
+    }
+    
+  }
+}
