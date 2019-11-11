569d8727cf86900dd6633763d9690068dd0fb980
devel@traal.ch <devel@traal.ch>
2019-11-11 22:17:05 +0100

    chore: Add ServletComponentScan annotation for ChatFilter.

--- src/main/java/edu/udacity/java/nano/WebSocketChatApplication.java
+++ src/main/java/edu/udacity/java/nano/WebSocketChatApplication.java
@@ -2,35 +2,22 @@ package edu.udacity.java.nano;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpServletRequest;
-import java.net.UnknownHostException;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
-@RestController
+@ServletComponentScan       // td, 10.11.2019: Used for ChatFilter.
 public class WebSocketChatApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(WebSocketChatApplication.class, args);
-    }
+  
+  /* member variables */
+  
+  /* constructors */
+  
+  /* methods */
+  public static void main(String[] args) {
+    SpringApplication.run(WebSocketChatApplication.class, args);
+  }
+  
+  
 
-    /**
-     * Login Page
-     */
-    @GetMapping("/")
-    public ModelAndView login() {
-        return new ModelAndView("/login");
-    }
-
-    /**
-     * Chatroom Page
-     */
-    @GetMapping("/index")
-    public ModelAndView index(String username, HttpServletRequest request) throws UnknownHostException {
-        //TODO: add code for login to chatroom.
-        return null;
-    }
 }
